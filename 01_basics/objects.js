68ac4db5aa2ef56f5d8493baafe2af4a7ef61e9c
Bhumika Verma <97393057+bhumi-17@users.noreply.github.com>
2024-10-18 17:21:27 +0000

    json_talk

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -32,7 +32,7 @@
 // console.log(jsuser.greeting())
 // console.log(jsuser.greeting2())
 
-const tinderuser={}
+const tinderuser={}//non-singleton-object
 tinderuser.id="123abc"
 tinderuser.name="sammy"
 tinderuser.isloggedin=false
@@ -48,11 +48,50 @@ const regularuser={
         }
     }
 }
-console.log(regularuser.fullname)
-const obj1={1:"a",2:"b"}
-const obj2={3:"a",4:"b"}
-// const obj3={obj1,obj2}
-const obj3=Object.assign({},obj1,obj2)
-const obj4={...obj1,...obj2}
-console.log(obj3)
-console.log(obj4)
+// console.log(regularuser.fullname)
+// const obj1={1:"a",2:"b"}
+// const obj2={3:"a",4:"b"}
+// // const obj3={obj1,obj2}
+// const obj3=Object.assign({},obj1,obj2)
+// const obj4={...obj1,...obj2}
+// console.log(obj3)
+// console.log(obj4)
+
+// const users=[
+//     {
+//         id:1,
+//         email:"bhumi@gmail.com"
+//     },
+//     {
+//         id:2,
+//         email:"abc@gmail.com"
+//     }
+// ];
+// console.log(users[1].email)
+// console.log(tinderuser)
+// console.log(Object.keys(tinderuser))
+// console.log(Object.values(tinderuser))
+// console.log(Object.entries(tinderuser))//array me array ayega ek key hogi dusri value 
+// console.log(tinderuser.hasOwnProperty('isloggedin'))
+
+const course={
+    
+        coursename:"js playlist",
+        price:"999",
+        courseInstructor:"bhumi"
+    }
+    //course.courseInstructor-->ase bhi extract krskte
+    //also dusra tareeka-->destructuring
+    const{courseInstructor}=course
+    console.log(courseInstructor)
+    //incase rename krna hua
+    const{courseInstructor:ins}=course
+    console.log(ins)//result same ayega
+
+    // react k case me
+    // const navbar =(props.company-->bydefault hota h toh destructing krke iskijagh {company} ye likhdete h )=>{
+
+    // }
+    // jab bhi call krengy
+    // navbar(company="bhumi")
+
